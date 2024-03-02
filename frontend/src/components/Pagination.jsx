cfe80ca9c0623ce4a069dc1b2d566dcae5c5a1d9
Manish Bonala <bonalamanish@gmail.com>
2024-03-02 18:03:06 +0530

    Update Pagination.jsx

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -8,7 +8,7 @@ function Pagination({ pageNo, setPageNo, userData }) {
   return (
     <div className='flex justify-center mt-5 gap-2'>
       {Array.from({ length: totalPages }, (_, index) => (
-        <button key={index + 1} onClick={() => handlePageChange(index + 1)} className={`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded ${pageNo === index + 1 ? 'bg-blue-500 text-white' : 'text-blue-700'}`}>
+        <button key={index + 1} onClick={() => handlePageChange(index + 1)} className={`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded`}>
           {index + 1}
         </button>
       ))}
